d85d36526824aaa2cd7e68ae609685712641b714
jayeshsawani1505 <162999881+jayeshsawani1505@users.noreply.github.com>
2024-10-18 21:42:31 +0530

    Update cloudinary.js

--- cloudinary.js
+++ cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 cloudinary.config({
-  cloud_name: 'drsuiuchs',
-  api_key: process.env.c_key,
-  api_secret: process.env.c_secret
+  cloud_name: 'diq9nxcdo',
+  api_key: 999817712761936,
+  api_secret: FFC9TqOexPdT_f2m-hqonAfDLNs
 });
 
 module.exports = cloudinary;
